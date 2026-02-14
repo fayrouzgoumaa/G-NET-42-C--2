09080eb500115473e8a013fc7a8a2ffca9ce00ae
fayro <fayro@FAYROUZGOUMAA>
2026-02-14 22:29:09 +0200

    Q1

--- G-NET-42-C--2/Program.cs
+++ G-NET-42-C--2/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //double d = 9.99;
+            //int x1 = (int)d;
+            //Console.WriteLine(x1);
+            //Explicit casting from double to int.
+            //9.99 becomes 9 because the fractional part is discarded.
+            #endregion
+          
+
         }
     }
 }
