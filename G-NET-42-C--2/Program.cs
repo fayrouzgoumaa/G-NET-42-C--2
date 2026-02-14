6fcb90fb2690830b920d668d40b140ae7f9549cb
fayro <fayro@FAYROUZGOUMAA>
2026-02-14 22:31:59 +0200

    Q1

--- G-NET-42-C--2/Program.cs
+++ G-NET-42-C--2/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             #region Q1
-            //double d = 9.99;
-            //int x1 = (int)d;
-            //Console.WriteLine(x1);
-            //Explicit casting from double to int.
-            //9.99 becomes 9 because the fractional part is discarded.
+            double d = 9.99;
+            int x1 = (int)d;
+            Console.WriteLine(x1);
+            Explicit casting from double to int.
+            9.99 becomes 9 because the fractional part is discarded.
             #endregion
           
 
